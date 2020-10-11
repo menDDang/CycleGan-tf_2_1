752c2f279a46fb848367e563fde5783e43d92d7f
menDDang <menddang2357@gmail.com>
2020-10-11 16:48:46 +0900

    modify padding

--- src/model/modules/conv2d.py
+++ src/model/modules/conv2d.py
@@ -2,12 +2,14 @@ import tensorflow as tf
 
 
 class Conv2D(tf.keras.layers.Layer):
+    '''
+        Note that this class does not apply padding
+        So, user must use external padding layer
+    '''
     def __init__(self, 
                 filters, 
                 kernel_size, 
                 strides=1, 
-                pad_type='ZEROS',
-                padding='SAME',
                 initializer=tf.random.uniform, 
                 **kwargs
                 ):
@@ -15,8 +17,6 @@ class Conv2D(tf.keras.layers.Layer):
         assert(filters > 0)
         assert(len(kernel_size) == 2)
         assert(strides >= 1)
-        assert(pad_type in ['ZEROS', 'REFLECT', 'SYMETRIC'])
-        assert(padding in ['SAME', 'NONE'])
 
         super(Conv2D, self).__init__(**kwargs)
 
@@ -24,14 +24,13 @@ class Conv2D(tf.keras.layers.Layer):
         self.filters = filters
         self.kernel_size = kernel_size
         self.strides = strides
-        self.pad_type = pad_type
-        self.padding = padding
         self.initializer = initializer
 
     def build(self, input_shapes):
         """
         input_shapes : [batch_size, height, width, input_channel_num]
         """
+        # Set kernel
         filter_shape = [self.kernel_size[0], self.kernel_size[1], input_shapes[3], self.filters]
         self.kernel = self.add_weight(
             shape=filter_shape,
@@ -43,29 +42,90 @@ class Conv2D(tf.keras.layers.Layer):
         # Set bias
         self.bias = self.add_weight(
             shape=[self.filters],
-            initializer=self.initializer,
+            initializer=tf.initializers.Zeros,
             dtype=self.dtype,
             name='bias'
         )
 
         # Set paddings
-        if self.padding == 'SAME':
-            # IH = OH = (IH + 2*PH - KH) / SH + 1
-            # So, we have PH = ((IH - 1) * SH + KH - IH) / 2 
-            pad_h = int(((input_shapes[1] - 1) * self.strides + self.kernel_size[0] - input_shapes[1]) / 2) + 1
-            pad_w = int(((input_shapes[2] - 1) * self.strides + self.kernel_size[1] - input_shapes[2]) / 2) + 1
-            self.padding_size = [[0, 0], [pad_h, pad_h], [pad_w, pad_w], [0, 0]]
+        self.padding = [[0, 0], [0, 0], [0, 0], [0, 0]]
 
         self.built = True
 
     def call(self, x):
-        # Apply padding
-        if self.padding == 'SAME':
-            if self.pad_type == 'ZEROS':
-                x = tf.pad(x, self.padding_size, mode='CONSTANT', constant_values=0, name='padding')
-            else:
-                x = tf.pad(x, self.padding_size, mode=self.pad_type, name='padding')
-        
         # Convolution
-        x = tf.nn.conv2d(x, self.kernel, strides=self.strides, padding='VALID', data_format='NHWC', name='conv_out') + self.bias
+        x = tf.nn.conv2d(x, self.kernel, strides=self.strides, padding=self.padding, data_format='NHWC', name='conv_out')
+        # Add bias
+        x += self.bias
+
         return x
+
+
+class Upsampling2D(tf.keras.layers.Layer):
+    '''
+        Note that this class apply 'VALID' padding
+        So, user must do not use external padding layer
+    '''
+    def __init__(self, filters, kernel_size, output_shape, strides=1, initializer=tf.random.uniform, **kwargs):
+        # Check arguments
+        assert(filters > 0)
+        assert(len(kernel_size) == 2)
+        assert(len(output_shape) == 2)
+        assert(strides >= 1)
+        
+        super(Upsampling2D, self).__init__(**kwargs)
+
+        # Set attributes
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.output_height = output_shape[0]
+        self.output_width = output_shape[1]
+        self.strides = strides
+        self.initializer = initializer
+
+    def build(self, input_shapes):
+        # Get shape of inputs
+        batch_size, _, _, input_channel_num = input_shapes  # [batch, height, width, channels]
+
+        # Set kernel
+        filter_shape = [self.kernel_size[0], self.kernel_size[1], self.filters, input_channel_num]
+        self.kernel = self.add_weight(
+            shape=filter_shape,
+            initializer=self.initializer,
+            dtype=self.dtype,
+            name='kernel'
+        )
+
+        # Set bias
+        self.bias = self.add_weight(
+            shape=[self.filters],
+            initializer=tf.initializers.Zeros,
+            dtype=self.dtype,
+            name='bias'
+        )
+
+        # Set output shape
+        self._output_shape = [batch_size, self.output_height, self.output_width, self.filters]
+
+        self.built = True
+
+    def call(self, x):
+        # Transposed convolution 2d
+        x = tf.nn.conv2d_transpose(x, self.kernel, output_shape=self._output_shape, strides=self.strides, padding='VALID', 
+            data_format='NHWC', name='conv_transpose_out')
+            
+        x += self.bias
+
+        return x
+
+
+if __name__ == "__main__":
+    x = tf.zeros(shape=[100, 28, 28, 3], dtype=tf.float32)
+
+    down_sampling = Conv2D(16, [8, 8], dtype=tf.float32)
+    up_sampling = Upsampling2D(16, [8, 8], output_shape=[28, 28], dtype=tf.float32)
+
+    x = down_sampling(x)
+    print(x.shape)
+    x = up_sampling(x)
+    print(x.shape)
--- src/model/modules/residual_conv2d.py
+++ src/model/modules/residual_conv2d.py
@@ -1,39 +1,72 @@
 import tensorflow as tf
 from .conv2d import Conv2D
 
-
 class ResidualConv2D(tf.keras.layers.Layer):
     def __init__(self, 
                 kernel_size, 
                 strides=1, 
                 pad_type='ZEROS',
-                padding='SAME',
                 initializer=tf.random.uniform, 
                 **kwargs
                 ):
+        # Check arguments
+        assert(len(kernel_size) == 2)
+        assert(strides > 0)
+        assert(pad_type in ['ZEROS', 'REFLECT', 'SYMETRIC'])
+        
         super(ResidualConv2D, self).__init__(**kwargs)
         
         self.kernel_size = kernel_size
         self.strides = strides
         self.pad_type = pad_type
-        self.padding = padding
         self.initializer = initializer
 
     def build(self, input_shapes):
-        """
-        input_shapes : [batch_size, height, width, input_channel_num]
-        """
-        self.conv = Conv2D(
-                filters=input_shapes[3],
+        # Get shape of inputs
+        _, input_height, input_width, input_channel_num = input_shapes
+
+        # Set padding size
+        # IH = OH = (IH + (PH_left + PH_right) - KH) / SH + 1
+        # PH_left + PH_right = (IH - 1) * SH - IH + KH
+        pad_height = (input_height - 1) * self.strides + - input_height + self.kernel_size[0]
+        if pad_height % 2 == 0:
+            ph_left = int(pad_height / 2)
+            ph_right = int(pad_height / 2)
+        else:
+            ph_left = int(pad_height / 2)
+            ph_right = int(pad_height / 2) + 1
+        pad_width = (input_width - 1) * self.strides - input_width + self.kernel_size[1]
+        if pad_width % 2 == 0:
+            pw_left = int(pad_width / 2)
+            pw_right = int(pad_width / 2)
+        else:
+            pw_left = int(pad_width / 2)
+            pw_right = int(pad_width / 2) + 1
+
+        self.padding_size = [[0, 0], [ph_left, ph_right], [pw_left, pw_right], [0, 0]]
+        
+        # Set convolution layer
+        self.conv = conv2d.Conv2D(
+                filters=input_channel_num,
                 kernel_size=self.kernel_size, 
                 strides=self.strides, 
-                pad_type=self.pad_type,
-                padding=self.padding,
                 initializer=self.initializer, 
                 dtype=self.dtype,
                 name='conv'
                 )
+
         self.built = True
 
     def call(self, x):
-        return self.conv(x) + x
+        if self.pad_type == 'ZEROS':
+            padded_x = tf.pad(x, self.padding_size, mode='CONSTANT', constant_values=0, name='padding')
+        else:
+            padded_x = tf.pad(x, self.padding_size, mode=self.pad_type, name='padding')
+        
+        return self.conv(padded_x) + x
+
+if __name__ == "__main__":
+    x = tf.zeros(shape=[100, 28, 28, 3], dtype=tf.float32)
+    resnet = ResidualConv2D([8, 8], dtype=tf.float32)
+    x = resnet(x)
+    print(x.shape)
